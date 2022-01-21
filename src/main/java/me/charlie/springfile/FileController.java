3b524e2cfe1cb6df7a7f672aac58a50546d4ac9d
gomding <backchida@gmail.com>
2022-01-22 02:05:10 +0900

    feat: 파일 다운로드 기능 구현

--- src/main/java/me/charlie/springfile/FileController.java
+++ src/main/java/me/charlie/springfile/FileController.java
@@ -1,12 +1,20 @@
 package me.charlie.springfile;
 
+import org.apache.tika.Tika;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class FileController {
@@ -26,5 +34,24 @@ public class FileController {
     public String fileUpload(@RequestBody MultipartFile file, RedirectAttributes attributes) {
         attributes.addFlashAttribute("filename", fileService.fileUpload(file));
         return "redirect:/files";
+    }
+
+    @GetMapping("/file")
+    public String fileDownloadForm() {
+        return "files/download";
+    }
+
+    @GetMapping("/file/download")
+    public ResponseEntity<Resource> fileDownload(@RequestParam String filename) throws IOException {
+        Resource resource = fileService.fileDownload(filename);
+        File file = resource.getFile();
+
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename:\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                .body(resource);
     }
 }
--- src/main/java/me/charlie/springfile/FileService.java
+++ src/main/java/me/charlie/springfile/FileService.java
@@ -1,5 +1,7 @@
 package me.charlie.springfile;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,10 +12,17 @@ import java.nio.file.Paths;
 @Service
 public class FileService {
 
-    private static final String FILE_PATH = "src/main/resources/file";
+    private static final String FILE_UPLOAD_PATH = "src/main/resources/file/";
+    private static final String FILE_DOWNLOAD_PATH = "classpath:/file/";
+
+    private final ResourceLoader resourceLoader;
+
+    public FileService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     public String fileUpload(MultipartFile file) {
-        Path path = Paths.get(FILE_PATH).toAbsolutePath().normalize();
+        Path path = Paths.get(FILE_UPLOAD_PATH).toAbsolutePath().normalize();
         String filename = file.getOriginalFilename();
         Path targetPath = path.resolve(filename).normalize();
         try {
@@ -22,5 +31,9 @@ public class FileService {
             throw new IllegalArgumentException("파일 업로드에 실패했습니다.");
         }
         return filename;
+    }
+
+    public Resource fileDownload(String filename) {
+        return resourceLoader.getResource(FILE_DOWNLOAD_PATH + filename);
     }
 }
